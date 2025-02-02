23d4f403ace27fe738353dfb15b077381b24040f
amas <andrew.mascioli1@gmail.com>
2025-02-01 22:02:18 -0500

    Add FK relationship between status and battery_info

--- db.py
+++ db.py
@@ -13,14 +13,17 @@ class Column:
     type: Literal["TEXT", "NUMERIC", "INTEGER", "REAL", "BLOB"]
     primary_key: bool = False
     nullable: bool = False
+    autoincrement: bool = False
 
     @property
     def spec(self) -> str:
         statement = f"{self.name} {self.type}"
         if self.primary_key:
-            statement = f"{statement} NOT NULL"
-        elif self.nullable:
             statement = f"{statement} PRIMARY KEY"
+            if self.autoincrement:
+                statement = f"{statement} AUTOINCREMENT"
+        elif not self.nullable:
+            statement = f"{statement} NOT NULL"
         return statement
 
 
@@ -28,29 +31,21 @@ class Column:
 class Table:
     name: str
     columns: tuple[Column, ...]
+    additional_statements: str = ""
 
     @property
     def create_statement(self) -> str:
-        column_stms = ", ".join(col.spec for col in self.columns)
-        return f"CREATE TABLE IF NOT EXISTS {self.name} ({column_stms})"
+        column_stmnts = ", ".join(col.spec for col in self.columns)
+        if self.additional_statements:
+            column_stmnts = f"{column_stmnts}, {self.additional_statements}"
+        return f"CREATE TABLE IF NOT EXISTS {self.name} ({column_stmnts})"
 
 
 class Database:
-    STATUS_TABLE = Table(
-        "status",
-        (
-            Column("timestamp_utc", "INTEGER", primary_key=True),
-            Column("status", "INTEGER"),
-            Column("voltage", "INTEGER"),
-            Column("power", "INTEGER"),
-            Column("energy_full", "INTEGER"),
-            Column("energy_now", "INTEGER"),
-        ),
-    )
     BATTERY_INFO_TABLE = Table(
         "battery_info",
         (
-            Column("id", "INTEGER", primary_key=True),
+            Column("id", "INTEGER", primary_key=True, autoincrement=True),
             Column("voltage_min_design", "INTEGER"),
             Column("energy_full_design", "INTEGER"),
             Column("model_name", "TEXT"),
@@ -58,7 +53,20 @@ class Database:
             Column("serial_number", "TEXT"),
         ),
     )
-    TABLES = (STATUS_TABLE, BATTERY_INFO_TABLE)
+    STATUS_TABLE = Table(
+        "status",
+        (
+            Column("timestamp_utc", "INTEGER", primary_key=True),
+            Column("info_id", "INTEGER"),
+            Column("status", "INTEGER"),
+            Column("voltage", "INTEGER"),
+            Column("power", "INTEGER"),
+            Column("energy_full", "INTEGER"),
+            Column("energy_now", "INTEGER"),
+        ),
+        additional_statements=f"FOREIGN KEY(info_id) REFERENCES {BATTERY_INFO_TABLE.name}(id)",
+    )
+    TABLES = (BATTERY_INFO_TABLE, STATUS_TABLE)
 
     def __init__(self, path: Path):
         self.path = path
@@ -78,10 +86,19 @@ class Database:
             for table in Database.TABLES:
                 cur.execute(table.create_statement)
 
+    def turn_on_foreign_keys(self):
+        with self.cursor() as cur:
+            cur.execute("PRAGMA foreign_keys = ON")
+
     def insert_battery_status(self, info: BatteryInfo, timestamp: int):
+        if (info_id := self.get_existing_battery_info_id(info)) is None:
+            self.insert_battery_info(info)
+            info_id = self.get_existing_battery_info_id(info)
+
         column_names = [col.name for col in self.STATUS_TABLE.columns]
         values = [
             timestamp,
+            info_id,
             info.status.value,
             # Convert units order of magnitude from micro- to mili-
             info.voltage_now // 1000,
@@ -98,8 +115,25 @@ class Database:
             cursor.execute(insert_stmt, values)
         self.conn.commit()
 
+    def get_existing_battery_info_id(self, info: BatteryInfo) -> int | None:
+        """Assume that two batteries are the same if they have
+        the same model_name + manufacturer + serial number"""
+        stmt = (
+            f"SELECT id FROM {self.BATTERY_INFO_TABLE.name} WHERE "
+            f"model_name = ? AND manufacturer = ? and serial_number = ?"
+        )
+        params = (info.model_name, info.manufacturer, info.serial_number)
+        with self.cursor() as cur:
+            cur.execute(stmt, params)
+            if result := cur.fetchone():
+                return result[0]
+            else:
+                return None
+
     def insert_battery_info(self, info: BatteryInfo):
-        column_names = [col.name for col in self.BATTERY_INFO_TABLE.columns]
+        column_names = [
+            col.name for col in self.BATTERY_INFO_TABLE.columns if not col.autoincrement
+        ]
         values = [
             # Convert units order of magnitude from micro- to mili-
             info.voltage_min_design // 1000,
